b609c695615ff99e2742fa2ef7b4160716d3ed2c
Mathias Arens <mathias@arens-festers.de>
2021-09-25 06:58:29 +0200

    Trying to show data failed

--- overview/OverviewScreen.js
+++ overview/OverviewScreen.js
@@ -1,21 +1,24 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import { Button, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from "react";
+import { Button, ScrollView } from "react-native";
 import { useTranslation } from "react-i18next";
-import { authenticatedFetch, handleAuthenticationError } from '../authentication/authenticatedFetch';
-import { useToast } from "native-base";
-import {AuthContext} from '../authentication/AuthContext';
+import {
+  authenticatedFetch,
+  handleAuthenticationError,
+} from "../authentication/authenticatedFetch";
+import { Box, useToast, VStack,Text } from "native-base";
+import { AuthContext } from "../authentication/AuthContext";
 
 export default function OverviewScreen({ navigation }) {
   const { t } = useTranslation();
   const toast = useToast();
   const { signOut } = React.useContext(AuthContext);
-  const [ bookingSumPerTenant, setBookingSumPerTenants] = useState([]);
+  const [bookingSumPerTenants, setBookingSumPerTenants] = useState([]);
 
   const loadTenantBookingOverview = useCallback(() => {
-    authenticatedFetch('/tenant-booking-overview', signOut, {
-      method: 'GET',
+    authenticatedFetch("/tenant-booking-overview", signOut, {
+      method: "GET",
       headers: {
-        Accept: 'application/json',
+        Accept: "application/json",
       },
     })
       .then((response) => {
@@ -27,21 +30,26 @@ export default function OverviewScreen({ navigation }) {
       .catch((error) => {
         console.error(error);
         toast.show({
-          title: t(handleAuthenticationError(error))
+          title: t(handleAuthenticationError(error)),
         });
       });
   }, [t, navigation]);
-  
+
   useEffect(() => {
     loadTenantBookingOverview();
-  },[]);
+  }, []);
 
   return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          onPress={() => loadTenantBookingOverview()}
-          title="load"
-        />
-      </View>
-    );
-  }
+    <ScrollView>
+      <Box flex={1} p={2} w="90%" mx="auto">
+        {bookingSumPerTenants.map((bookingSumPerTenantItem) => {
+          <VStack space={2} mt={5}>
+            <Text>Hallo</Text>
+            <Text>{bookingSumPerTenantItem.tenant.name}</Text>
+          </VStack>
+        })}
+      </Box>
+      <Button onPress={() => loadTenantBookingOverview()} title="load" />
+    </ScrollView>
+  );
+}
