701f910592446210d35286c1014d9d1281018110
Mathias Arens <mathias@arens-festers.de>
2022-08-24 07:24:15 +0200

    Little updates to how results are presented

--- src/screens/synchronization/SynchronizationScreen.js
+++ src/screens/synchronization/SynchronizationScreen.js
@@ -20,6 +20,7 @@ import {
   Skeleton,
 } from "native-base";
 import sub from "date-fns/sub";
+import format from "date-fns/format";
 
 const initialState = { expectedSyncResults: [], syncResults: [] };
 
@@ -167,69 +168,70 @@ export default function SynchronizationScreen({ navigation }) {
       safeArea
     >
       <Box px={4}>
-      <VStack>
-        {accountSettingsItems.map((accountSettingsItem, index) => (
-          <HStack space={2} justifyContent="space-between" py={1}>
-            <Text>{accountSettingsItem.name}</Text>
+        <VStack>
+          {accountSettingsItems.map((accountSettingsItem, index) => (
+            <HStack space={2} justifyContent="space-between" py={1}>
+              <Text>{accountSettingsItem.name}</Text>
+            </HStack>
+          ))}
+        </VStack>
+        <VStack mb={4}>
+          <HStack space={2} justifyContent="space-between">
+            <Button
+              isLoading={
+                synchronizationButtonActive &&
+                syncState.expectedSyncResults.length > 0
+              }
+              onPress={sync}
+            >
+              {t("overviewScreenSyncAccounts")}
+            </Button>
           </HStack>
-        ))}
-      </VStack>
-      <VStack mb={4}>
-        <HStack space={2} justifyContent="space-between">
-          <Button
-            isLoading={
-              synchronizationButtonActive &&
-              syncState.expectedSyncResults.length > 0
-            }
-            onPress={sync}
-          >
-            {t("overviewScreenSyncAccounts")}
-          </Button>
-        </HStack>
-      </VStack>
-      <VStack>
-        {syncState.syncResults.map((syncResult, index) => (
-          <Box key={`synResult${index}`}>
-            <Text>{syncResult.accountName}</Text>
-
-            <FlatList
-              data={syncResult.newBookings}
-              renderItem={({ item }) => (
-                <Box px={4} py={2}>
-                  <HStack justifyContent="space-between">
+        </VStack>
+        <VStack>
+          {syncState.syncResults.map((syncResult, syncIndex) => (
+            <VStack key={`synResult${syncIndex}`}>
+              <Text>{syncResult.accountName}</Text>
+              <VStack>
+                {syncResult.newBookings.map((booking, bookingIndex) => (
+                  <HStack
+                    key={`booking${syncIndex}${booking.id}`}
+                    justifyContent="space-between"
+                  >
                     <Flex alignItems="flex-start">
-                      <Text>{item.date}</Text>
+                      <Text>
+                        {format(new Date(booking.date), t("dateFormat"))}
+                      </Text>
                     </Flex>
                   </HStack>
-                </Box>
-              )}
-              keyExtractor={(booking) =>
-                `${syncResult.accountName}${booking.id.toString()}`
-              }
-            />
-            <FlatList
-              data={syncResult.unmatchedTransactions}
-              renderItem={({ item }) => (
-                <Box px={4} py={2}>
-                  <HStack justifyContent="space-between">
-                    <Flex alignItems="flex-start">
-                      <Text>{item.date}</Text>
-                    </Flex>
-                  </HStack>
-                </Box>
-              )}
-              keyExtractor={(transaction) =>
-                `${syncResult.accountName}${transaction.id.toString()}`
-              }
-            />
-          </Box>
-        ))}
-      </VStack>
-      <VStack>
-        {syncState.expectedSyncResults.map((item, index) => (
-          <Skeleton.Text key={`expectedSyncResults${index}`} />
-        ))}
-      </VStack>
+                ))}
+              </VStack>
+              <VStack>
+                {syncResult.unmatchedTransactions.map(
+                  (transaction, transactionIndex) => (
+                    <HStack
+                      key={`${
+                        syncResult.accountName
+                      }${transaction.id.toString()}`}
+                      justifyContent="space-between"
+                    >
+                      <Flex alignItems="flex-start">
+                        <Text>
+                          {format(new Date(transaction.date), t("dateFormat"))}
+                        </Text>
+                      </Flex>
+                    </HStack>
+                  )
+                )}
+              </VStack>
+            </VStack>
+          ))}
+        </VStack>
+        <VStack>
+          {syncState.expectedSyncResults.map((item, index) => (
+            <Skeleton.Text key={`expectedSyncResults${index}`} />
+          ))}
+        </VStack>
       </Box>
     </Box>
   );
